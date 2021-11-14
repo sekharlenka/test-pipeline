02a5a2db647a467fdd4e8ec70c5a12204b28071f
soujanya-kotla <soujanya.kotla@gmail.com>
2021-11-14 14:56:42 -0500

    initial commit 1.20

--- com/test/target_data_loading.py
+++ com/test/target_data_loading.py
@@ -28,12 +28,14 @@ if __name__ == '__main__':
     for tgt in target_list:
         tgt_conf = app_conf[tgt]
 
-        src_data = tgt_conf["source_data"]
-        print("\nReading data from S3 Bucket using org.apache.hadoop:hadoop-aws:2.7.4")
-        cp_df = spark.read \
-                .parquet("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["staging_dir"] + "/" + src_data)
-        cp_df.show()
-        cp_df.createOrReplaceTempView("CP")
+        src_list = tgt_conf["source_data"]
+        for src_data in src_list:
+
+            print("\nReading data from S3 Bucket using org.apache.hadoop:hadoop-aws:2.7.4")
+            stg_df = spark.read \
+                    .parquet("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["staging_dir"] + "/" + src_data)
+            stg_df.show()
+            stg_df.createOrReplaceTempView(src_data)
         if tgt == 'REGIS_DIM':
             cp_reg_tgt_df =spark.sql(tgt_conf["loadingQuery"])
 
@@ -47,6 +49,7 @@ if __name__ == '__main__':
                 .option("dbtable", "DATAMART.REGIS_DIM") \
                 .mode("overwrite")\
                 .save()
+
         if tgt == 'CHILD_DIM':
 
             cp_chld_tgt_df = spark.sql(tgt_conf["loadingQuery"])
@@ -61,5 +64,29 @@ if __name__ == '__main__':
                 .mode("overwrite") \
                 .save()
 
+        if tgt == 'RTL_TXN_FCT':
+
+            dim_df = spark.read \
+                .format("io.github.spark_redshift_community.spark.redshift") \
+                .option("url", jdbc_url) \
+                .option("query", app_conf["redshift_conf"]["query"]) \
+                .option("forward_spark_s3_credentials", "true") \
+                .option("tempdir", "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/temp") \
+                .load()
+
+            dim_df.show(5, False)
+
+            dim_df.createOrReplaceTempView(gt_conf["target_src_table"])
+
+            fct_tgt_df =spark.sql(tgt_conf["loadingQuery"])
+            print("Writing txn_fact dataframe to AWS Redshift Table   >>>>>>>")
+            fct_tgt_df.coalesce(1).write\
+                .format("io.github.spark_redshift_community.spark.redshift") \
+                .option("url", jdbc_url) \
+                .option("tempdir", "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/temp") \
+                .option("forward_spark_s3_credentials", "true") \
+                .option("dbtable", tgt_conf["tableName"]) \
+                .mode("overwrite")\
+                .save()
 
 # spark-submit --master yarn --jars "https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.36.1060/RedshiftJDBC42-no-awssdk-1.2.36.1060.jar" --packages "org.apache.spark:spark-avro_2.11:2.4.2,io.github.spark-redshift-community:spark-redshift_2.11:4.0.1,org.apache.hadoop:hadoop-aws:2.7.4" com/test/target_data_loading.py
