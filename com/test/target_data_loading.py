61074e9d918127a4b43b576dafbb60cbabedc840
soujanya-kotla <soujanya.kotla@gmail.com>
2021-11-14 10:33:04 -0500

    initial commit 1.20

--- com/test/target_data_loading.py
+++ com/test/target_data_loading.py
@@ -26,15 +26,20 @@ if __name__ == '__main__':
 
     target_list = app_conf['target_list']
     for tgt in target_list:
-
-        src_conf = app_conf[tgt]["source_data"]
-
+        tgt_conf = app_conf[tgt]
         if tgt == 'REGIS_DIM':
+            src_data = tgt_conf["source_data"]
             print("\nReading data from S3 Bucket using org.apache.hadoop:hadoop-aws:2.7.4")
-            finance_df = spark.read \
-                .csv("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["staging_dir"] + "/" + src_conf + "/") \
-                .withColumn("ins_dt", current_date())
+            cp_df = spark.read \
+                .parquet("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["staging_dir"] + "/" + src_data)
 
-            finance_df.show()
+            cp_df.createOrReplaceTempView("CP")
+
+            spark.sql(tgt_conf["loadingQuery"]).show()
+
+
+
+
+
 
     # spark-submit --packages "mysql:mysql-connector-java:8.0.15,com.springml:spark-sftp_2.11:1.1.1,org.mongodb.spark:mongo-spark-connector_2.11:2.4.1,org.apache.hadoop:hadoop-aws:2.7.4" com/test/trget_data_loading.py
