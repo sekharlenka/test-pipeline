edf3dda1325d5e3831a0f9f82e17e8c08fbd3567
soujanya-kotla <soujanya.kotla@gmail.com>
2021-11-20 11:28:59 -0500

    initial commit 1.20

--- com/test/target_data_loading.py
+++ com/test/target_data_loading.py
@@ -70,7 +70,7 @@ if __name__ == '__main__':
             dim_df = spark.read \
                 .format("io.github.spark_redshift_community.spark.redshift") \
                 .option("url", jdbc_url) \
-                .option("query", app_conf["redshift_conf"]["query"]) \
+                .option("dbtable", tgt_conf["target_src_table"]) \
                 .option("forward_spark_s3_credentials", "true") \
                 .option("tempdir", "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/temp") \
                 .load()
